1e8838553930ec7fd6d48a2316143621f8d66827
vinay <vinayreddy2126@gmail.com>
2024-09-25 19:03:11 +0530

    branch switch

--- first.java
+++ first.java
@@ -1,6 +1,7 @@
 public class first
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         System.out.println("hello");
         System.out.println("welcome");
         System.out.println("to my github");
