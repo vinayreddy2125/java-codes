99ba75b2603f1a718a058c21f08f981fc4aad20e
vinay <vinayreddy2126@gmail.com>
2024-09-25 18:44:01 +0530

    adding of modified code to remote repository

--- first.java
+++ first.java
@@ -3,7 +3,7 @@ public class first
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("welcome");
-
+        System.out.println("to my github");
         
     }
 }
